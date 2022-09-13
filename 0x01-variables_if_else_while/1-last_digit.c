3f3823b4534ef487d44e67a0278e561ae4d1be9d
tebogo <mokolometsa@gmail.comgit config --global user.email mokolometsa@gmail.comgit config --global user.email mokolometsa@gmail.com>
2022-09-13 00:28:29 -0700

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,22 +13,21 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-      
-        printf("Last digit of %i is %i and is ", n, (n %10));
 
-        if ((n % 10) == 0)
+	printf("last digit of %i is %i ", n, (n % 10));
+
+	if ((n % 10) == 0)
 	{
-	printf("0\n");
+		printf("0\n");
 	}
 	else if ((n % 10) > 5)
 	{
-	printf("greater than 5\n");
+		printf("greater than 5\n");
 	}
 	else
 	{
-	printf("less than 6 and not 0\n");
+		printf("less than 6 and 0\n");
 	}
-        
-        return (0);
-}	
-	
+
+	return (0);
+}
