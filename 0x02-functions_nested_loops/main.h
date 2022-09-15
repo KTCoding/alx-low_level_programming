eeb8390533fa8fa7bd02f12746a95b8789e0f5b9
tebogo <mokolometsa@gmail.comgit config --global user.email mokolometsa@gmail.comgit config --global user.email mokolometsa@gmail.com>
2022-09-15 07:05:54 -0700

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,13 @@
 
 int _putchar(char c);
 int main(void);
+
 void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ */
+
 void print_alphabet_x10(void);
 
 #endif
